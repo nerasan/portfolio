f3052565899210088a8d634afa605c20d1aee07a
nerasan <salima.harun@gmail.com>
2021-02-03 18:59:39 -0600

    added resume

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Layout = (props) => {
     return (
@@ -12,7 +12,7 @@ const Layout = (props) => {
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><Link to="/about" className="nav-text black-text">about</Link></li>
                     <li><Link to="/projects" className="nav-text black-text">projects</Link></li>
-                    {/* <li><Link to="/resume" className="nav-text black-text">resume</Link></li> */}
+                    <li><a href="https://drive.google.com/file/d/1Lxcga0Z1TeB7ki1LYpp9V023PjHu1ymT/view" class="nav-text black-text">resume</a></li>
                 </ul>
                 </div>
             </nav>
@@ -24,10 +24,10 @@ const Layout = (props) => {
                 <div className="footer-copyright indigo lighten-5 black-text black">
                     <div className="container">
                         <a href="https://github.com/nerasan">
-                            <img src="https://image.flaticon.com/icons/png/512/25/25231.png" class="footer-icon" />
+                            <img src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="github icon" class="footer-icon" />
                         </a>
                         <a href="https://www.linkedin.com/in/salimaharun/">
-                            <img src="https://www.shareicon.net/data/2016/07/08/116977_address-book_512x512.png" class="footer-icon" />
+                            <img src="https://www.shareicon.net/data/2016/07/08/116977_address-book_512x512.png" alt="linkedin icon" class="footer-icon" />
                         </a>
                         <div>
                             © 2021 salima harun
