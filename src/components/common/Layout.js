3eb85bf85acafb99d0916d840536bb360e721507
nerasan <salima.harun@gmail.com>
2022-09-14 15:03:51 -0500

    update resume and links

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -12,7 +12,7 @@ const Layout = (props) => {
                 <ul id="nav-mobile" className="right">
                     <li><Link to="/about" className="nav-text black-text">about</Link></li>
                     <li><Link to="/projects" className="nav-text black-text">projects</Link></li>
-                    <li><a href="https://drive.google.com/file/d/1XZPOodZXUbaHAL7p3aDyb3knA1eDebdL/view?usp=sharing" className="nav-text black-text" target="_blank" rel="noreferrer noopener">resume</a></li>
+                    <li><a href="https://drive.google.com/file/d/1NlZTogS5xu_pOETXTLqrYQK-NnlUPw97/view" className="nav-text black-text" target="_blank" rel="noreferrer noopener">resume</a></li>
                 </ul>
                 </div>
             </nav>
@@ -23,10 +23,10 @@ const Layout = (props) => {
             <footer className="page-footer indigo lighten-5">
                 <div className="footer-copyright indigo lighten-5 black-text">
                     <div className="container">
-                        <a href="https://github.com/nerasan">
+                        <a href="https://github.com/nerasan" target="_blank" rel="noreferrer noopener">
                             <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github icon" className="footer-icon" />
                         </a>
-                        <a href="https://www.linkedin.com/in/salimaharun/">
+                        <a href="https://www.linkedin.com/in/salimaharun/" target="_blank" rel="noreferrer noopener">
                             <img src="https://www.shareicon.net/data/2016/07/08/116977_address-book_512x512.png" alt="linkedin icon" className="footer-icon" />
                         </a>
                         <div>
